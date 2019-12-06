19fb1ef6c7bed6f7c9aaf8bd732c74e40c329753
Karl Lew <karl@firepick.org>
2019-12-05 19:36:58 -0800

    lagrange

--- src/tracker.js
+++ src/tracker.js
@@ -9,86 +9,82 @@
             this.sampleRate = opts.sampleRate || 3;
         }
 
-        phase(waveform, sampleRate) {
+        xIntercept3(pts) {
+            if (pts.length !== 3) {
+                throw new Error(`Expected three points {x,y}`);
+            }
+
+            // Lagrange interpolation
+            //y1(x−x2)(x−x3)/(x1−x2)(x1−x3) +
+            //y2(x−x1)(x−x3)/(x2−x1)(x2−x3) +
+            //y3(x−x1)(x−x2)/(x3−x1)(x3−x2)
+
+            var { x:x1, y:y1, } = pts[0];
+            var { x:x2, y:y2, } = pts[1];
+            var { x:x3, y:y3, } = pts[2];
+
+            // Assume evenly spaced points so (x1-x2) = (x3-x2)
+            var a = y1 - 2*y2 + y3;    
+            var b = -y1*x2-y1*x3 +2*y2*x1+2*y2*x3 -y3*x1-y3*x2;
+            var c = y1*x2*x3 -2*y2*x1*x3 +y3*x1*x2;
+            var rootbac = Math.sqrt(b*b - 4*a*c);
+            var xa = (-b-rootbac) / (2*a);
+            var xb = (-b+rootbac) / (2*a);
+
+            // return solution within interval
+            return ((x1 <= xa && xa <= x3) ? xa : xb);
+        }
+
+        analyze(waveform, sampleRate) {
             var zeroes = [];
-            var wi2 = waveform[0];
-            var wi1 = waveform[1];
-            var sum = 0;
-            var sum3 = 0;
-            var n = 0;
-            var iPhase = undefined;
-            for (var i=2; i<waveform.length; i++) {
+            var sampleInterval = 1/sampleRate;
+            var wi3 = waveform[0];
+            var wi2 = waveform[1];
+            var wi1 = waveform[2];
+            var phaseDelay = undefined;
+            for (var i=3; i<waveform.length; i++) {
                 var wi = waveform[i];
-                if (wi1<=0 && wi>0 || wi1>0 && wi<=0) {
-                    if (wi1 < 0 && iPhase === undefined) {
-                        iPhase = i;
+                if (wi2<=0 && wi1>0 || wi2>0 && wi1<=0) {
+                    var pts = [{
+                        x: (i-3)*sampleInterval,
+                        y: wi3,
+                    },{
+                        x: (i-2)*sampleInterval,
+                        y: wi2,
+                    },{
+                        x: (i-1)*sampleInterval,
+                        y: wi1,
+                    }];
+                    var t = this.xIntercept3(pts);
+                    if (wi2 <= 0 && phaseDelay === undefined) {
+                        phaseDelay = t;
                     }
-                    let diz = wi1 / (wi1 - wi);
-                    let diz21 = wi2 / (wi2 - wi1);
-                    let diz2 = wi2 / (wi2 - wi);
-                    console.log(`dbg diz`, 
-                        js.s({wi2, wi1, wi, diz, diz21, diz2}));
-                    //let diz3 = (diz + diz21 + diz2)/3;
-                    let diz3 = (diz + diz2)/2;
-                    sum3 += diz3;
-                    sum += diz;
-                    n++;
-                    zeroes.push({
-                        i,
-                        sum,
-                        sum3,
-                        n,
-                        avg: sum / n,
-                        avg3: sum3 / n,
-                        t: (i-diz)/sampleRate,
-                        t3: (i-diz3)/sampleRate,
-                    });
+                    zeroes.push({ i, t, });
                 }
+                wi3 = wi2;
                 wi2 = wi1;
                 wi1 = wi;
             }
             zeroes.forEach(z => console.log(js.s(z)));
             var zStats = zeroes.reduce((a,z,i) => {
-                if (i === 0) {
-                    a.tSum = 0;
-                    a.tSum3 = 0;
-                } else {
-                    a.tSum += z.t - a.zPrev.t;
-                    a.tSum3 += z.t3 - a.zPrev.t3;
-                }
+                a.tSum = i === 0 
+                    ? a.tSum = 0
+                    : a.tSum += z.t - a.zPrev.t;
                 a.zPrev = z;
                 return a;
             },{});
             var period = 2 * zStats.tSum/(zeroes.length-1);
-            var period3 = 2 * zStats.tSum3/(zeroes.length-1);
-            var frequency = 1/period;
-            var phaseDelay = iPhase/sampleRate;
-            var duration = waveform.length / sampleRate;
-            //console.log(`dbg zeroes`, waveform, zeroes);
+            var sampleTime = waveform.length / sampleRate;
             return {
                 period, 
-                period3,
-                frequency,
                 phaseDelay,
-                duration,
+                sampleTime,
+                sampleRate,
+                sampleInterval,
+                zeroes: zeroes.length,
             }
         }
 
-        frequency(waveform, sampleRate) {
-            sampleRate = sampleRate || this.sampleRate;
-            var spectrum = fourier(waveform);
-            var dHz = sampleRate/waveform.length;
-            var db = spectrum.map(v => decibels.fromGain(v));
-            var iMax = db.reduce((a,d,i) => (db[i] > db[a] ? i : a), 0);
-            if (iMax < 1 || waveform.length-2 < iMax) {
-                throw new Error(
-                    `Cannot compute peak frequency iMax:${iMax}`);
-            }
-            // parabolic estimate for peak frequency
-            var [dba, dbb, dbc] = [db[iMax-1], db[iMax], db[iMax+1]];
-            var p = 0.5 * (dba-dbc) / (dba - 2 * dbb + dbc);
-            return (iMax + p) * dHz + dHz/2;
-        }
     }
 
     module.exports = exports.Tracker = Tracker;
